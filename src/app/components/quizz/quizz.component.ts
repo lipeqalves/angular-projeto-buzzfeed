a64e2923e41d7dd96d197d6f4ec4e9c2320c3cda
Filipe Alves <lipeqa@gmail.com>
2023-04-24 19:43:37 -0300

    Criando as regras do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
@@ -17,7 +17,33 @@ export class QuizzComponent {
   questionIndex: number = 0;
   questionMaxIndex: number = 0;
 
-  finished:boolean = false;
+  finished: boolean = true;
+  constructor() { }
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false;
+      this.title = quizz_questions.title;
 
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+
+    }
+  }
+  playerChoose(value: string) {
+    this.answers.push(value);
+
+  }
+  async nextStep() {
+    this.questionIndex += 1;
+    if (this.questionMaxIndex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex];
+    } else {
+      this.finished = true;
+    }
+
+  }
 
 }
