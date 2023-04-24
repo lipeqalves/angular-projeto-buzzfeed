535e740ebe167e0c42cae7cec22d90a0eca677f6
Filipe Alves <lipeqa@gmail.com>
2023-04-24 20:03:48 -0300

    Calculando o Resultado

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -34,16 +34,30 @@ export class QuizzComponent {
   }
   playerChoose(value: string) {
     this.answers.push(value);
-
+    this.nextStep()
   }
   async nextStep() {
     this.questionIndex += 1;
     if (this.questionMaxIndex > this.questionIndex) {
       this.questionSelected = this.questions[this.questionIndex];
     } else {
+      const finalAnswer: string = await this.checkResult(this.answers)
       this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
     }
 
+  }
+  async checkResult(anwsers: string[]) {
+    const result = anwsers.reduce((previous, current, i, arr) => {
+      if (
+        arr.filter(item => item === previous).length > arr.filter(item => item === current).length
+      ) {
+        return previous;
+      } else {
+        return current;
+      }
+    })
+    return result;
   }
 
 }
